34d6388dbfcaa071740706a9be7453e0788f8638
Adland Lee <adland@4mfd.com>
2021-07-29 16:50:40 -0700

    add Random definition files

---
+++ blackjack_20180527_1941/Random.cpp
@@ -0,0 +1,9 @@
+//
+//  Random.cpp
+//  blackjack_20180527_1941
+//
+//  Created by Adland Lee on 7/29/21.
+//  Copyright © 2021 4MFD. All rights reserved.
+//
+
+#include "Random.hpp"
---
+++ blackjack_20180527_1941/Random.hpp
@@ -0,0 +1,14 @@
+//
+//  Random.hpp
+//  blackjack_20180527_1941
+//
+//  Created by Adland Lee on 7/29/21.
+//  Copyright © 2021 4MFD. All rights reserved.
+//
+
+#ifndef Random_hpp
+#define Random_hpp
+
+#include <stdio.h>
+
+#endif /* Random_hpp */
