4ccb146ed0bbef27baf137ba3c42b4a59dab8629
Adland Lee <adland@4mfd.com>
2021-07-29 20:25:39 -0700

    move gamedeck methods

--- blackjack_20180527_1941/GameDeck.cpp
+++ blackjack_20180527_1941/GameDeck.cpp
@@ -7,3 +7,39 @@
 //
 
 #include "GameDeck.hpp"
+
+#include <iostream>
+
+#include "Random.hpp"
+
+using namespace std;
+
+GameDeck::GameDeck(IntrinsicCardDeck* intrinsicDeck, int numberOfDecks) {
+    numberOfCards = numberOfDecks * (*intrinsicDeck).getCount();
+    vector<Card*> newShoe(numberOfCards);
+    
+    for (int i=0; i<numberOfCards; i++) {
+        int cardIndex = i % (*intrinsicDeck).getCount();
+        newShoe[i] = (*intrinsicDeck)[cardIndex];
+    }
+    shoe = newShoe;
+    shuffle();
+}
+
+void GameDeck::shuffle() {
+    std::shuffle(std::begin(shoe), std::end(shoe), Random::generator());
+    nextIndex = 0;
+}
+
+Card* GameDeck::deal() {
+    Card* card = shoe[nextIndex];
+    nextIndex += 1;
+    return card;
+}
+
+void GameDeck::drawDeck() {
+    for (int i=0; i < numberOfCards; i++) {
+        Card* card = shoe[i];
+        cout << card->description() << endl;
+    }
+}
